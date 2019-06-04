fc0a8a53f9c48912d859615bf17145dd4b74c03b
jjackson053 <ljjackson255@gmail.com>
2019-06-03 20:47:16 -0400

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CustomerFormComponentComponent } from './customer-form-component/customer-form-component.component';
+import { CustomerListComponentComponent } from './customer-list-component/customer-list-component.component';
+import { AccountFormComponentComponent } from './account-form-component/account-form-component.component';
+import { AccountListComponentComponent } from './account-list-component/account-list-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo: '/customer', pathMatch:'full'},
+  {path: 'addCustomer', component: CustomerFormComponentComponent },
+  {path: 'customer', component: CustomerListComponentComponent},
+  {path: 'addAccount', component: AccountFormComponentComponent},
+  {path: 'listAccount', component: AccountListComponentComponent},
+  {path: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccountListComponentComponent } from './account-list-component/account-list-component.component';
+import { CustomerFormComponentComponent } from './customer-form-component/customer-form-component.component';
+import { AccountFormComponentComponent } from './account-form-component/account-form-component.component';
+import { CustomerListComponentComponent } from './customer-list-component/customer-list-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountListComponentComponent,
+    CustomerFormComponentComponent,
+    AccountFormComponentComponent,
+    CustomerListComponentComponent
   ],
   imports: [
     BrowserModule,
